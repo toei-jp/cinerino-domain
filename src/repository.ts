6a8bc804237def70914b01594919223725fddd64
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-10-05 00:03:50 +0000

    # TOEI-4 ### Changed - テレメトリーサービスを外部化。

--- src/repository.ts
+++ src/repository.ts
@@ -7,16 +7,13 @@ import { MongoRepository as ActionRepo } from './repo/action';
 import { RedisRepository as CodeRepo } from './repo/code';
 import { RedisRepository as ConfirmationNumberRepo } from './repo/confirmationNumber';
 import { MongoRepository as EventRepo } from './repo/event';
-import { MongoRepository as GMONotificationRepo } from './repo/gmoNotification';
 import { MongoRepository as OrderRepo } from './repo/order';
 import { RedisRepository as OrderNumberRepo } from './repo/orderNumber';
 import { MongoRepository as OrganizationRepo } from './repo/organization';
 import { MongoRepository as OwnershipInfoRepo } from './repo/ownershipInfo';
 import { CognitoRepository as PersonRepo } from './repo/person';
 import { MongoRepository as ProgramMembershipRepo } from './repo/programMembership';
-import { MongoRepository as SendGridEventRepo } from './repo/sendGridEvent';
 import { MongoRepository as TaskRepo } from './repo/task';
-import { MongoRepository as TelemetryRepo } from './repo/telemetry';
 import { MongoRepository as TransactionRepo } from './repo/transaction';
 
 export class AccountNumber extends AccountNumberRepo { }
@@ -26,16 +23,13 @@ export namespace action {
 export class Code extends CodeRepo { }
 export class ConfirmationNumber extends ConfirmationNumberRepo { }
 export class Event extends EventRepo { }
-export class GMONotification extends GMONotificationRepo { }
 export class Order extends OrderRepo { }
 export class OrderNumber extends OrderNumberRepo { }
 export class Organization extends OrganizationRepo { }
 export class OwnershipInfo extends OwnershipInfoRepo { }
 export class Person extends PersonRepo { }
 export class ProgramMembership extends ProgramMembershipRepo { }
-export class SendGridEvent extends SendGridEventRepo { }
 export class Task extends TaskRepo { }
-export class Telemetry extends TelemetryRepo { }
 export class Transaction extends TransactionRepo { }
 export namespace itemAvailability {
 }
--- src/service/report.ts
+++ src/service/report.ts
@@ -1,12 +1,8 @@
 /**
  * レポートサービス
- * 基本的に実験的実装中です。
- * 通常保守で必要なものを実装したり、思いつきのままにやっています。
  */
-import * as HealthService from './report/health';
 import * as TransactionService from './report/transaction';
 
 export {
-    HealthService as health,
     TransactionService as transaction
 };
