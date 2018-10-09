5e0bb61954640685169f59b23f9f0e8681543c64
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-10-09 23:52:32 +0000

    # TOEI-4 ### Changed - 所有権コード期限をアプリ側で制御できるように変更。

--- src/repo/code.ts
+++ src/repo/code.ts
@@ -2,10 +2,10 @@ import * as createDebug from 'debug';
 import { RedisClient } from 'redis';
 import * as uuid from 'uuid';
 
-const debug = createDebug('cinerino-domain:repository');
-const REDIS_KEY_PREFIX = 'cinerino-domain:code';
-const CODE_EXPIRES_IN_SECONDS = 600;
+import * as factory from '../factory';
 
+const debug = createDebug('cinerino-domain:repository');
+const REDIS_KEY_PREFIX = 'cinerino:code';
 export type IData = any;
 export type ICode = string;
 /**
@@ -21,12 +21,20 @@ export class RedisRepository {
      */
     public async publish(params: {
         data: IData;
+        expiresInSeconds: number;
     }): Promise<ICode> {
         const code = uuid.v4();
-        await this.save({ code: code, data: params.data });
+        await this.save({
+            code: code,
+            data: params.data,
+            expiresInSeconds: params.expiresInSeconds
+        });
 
         return code;
     }
+    /**
+     * コードを削除する
+     */
     public async remove(params: { code: ICode }): Promise<void> {
         const key = `${REDIS_KEY_PREFIX}:${params.code}`;
         await new Promise<void>((resolve) => {
@@ -35,38 +43,45 @@ export class RedisRepository {
             });
         });
     }
+    /**
+     * コードでデータを検索する
+     */
     public async findOne(params: { code: ICode }): Promise<IData> {
         const key = `${REDIS_KEY_PREFIX}:${params.code}`;
 
-        return new Promise<any>((resolve, reject) => {
+        return new Promise<IData>((resolve, reject) => {
             this.redisClient.get(key, (err, value) => {
                 if (err instanceof Error) {
-                    reject();
-
-                    return;
+                    reject(err);
+                } else {
+                    if (value === null) {
+                        reject(new factory.errors.NotFound('Code'));
+                    } else {
+                        resolve(JSON.parse(value));
+                    }
                 }
-
-                resolve((value === null) ? null : JSON.parse(value));
             });
         });
     }
+    /**
+     * コードを保管する
+     */
     private async save(params: {
         code: ICode;
         data: IData;
+        expiresInSeconds: number;
     }): Promise<void> {
         const key = `${REDIS_KEY_PREFIX}:${params.code}`;
         await new Promise<void>((resolve, reject) => {
             this.redisClient.multi()
                 .set(key, JSON.stringify(params.data))
-                .expire(key, CODE_EXPIRES_IN_SECONDS, debug)
+                .expire(key, params.expiresInSeconds, debug)
                 .exec((err) => {
                     if (err instanceof Error) {
                         reject(err);
-
-                        return;
+                    } else {
+                        resolve();
                     }
-
-                    resolve();
                 });
         });
     }
